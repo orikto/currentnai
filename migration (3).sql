69cca438c3ee4d9275ec06bd90b2a794e676b000
orikto9975 <orikto9975@gmail.com>
2026-09-04 03:34:15 +0300

    Delete migration (3).sql

--- migration (3).sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS page_views (
-  id SERIAL PRIMARY KEY,
-  visit_date DATE NOT NULL DEFAULT CURRENT_DATE,
-  created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
-);
-CREATE INDEX IF NOT EXISTS idx_page_views_date ON page_views(visit_date);
